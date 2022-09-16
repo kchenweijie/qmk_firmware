cd19a4ecc613f28d29b62d6400a3599941aca688
Kevin Chen <kevin.chen.weijie@gmail.com>
2022-09-16 18:42:31 -0500

    organized keymaps

--- keyboards/keebio/iris/keymaps/kchenweijie/config.h
+++ keyboards/keebio/iris/keymaps/kchenweijie/config.h
@@ -1,23 +1,31 @@
-/*
-Copyright 2017 Danny Nguyen <danny@keeb.io>
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 2 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
 #pragma once
 
-// #define USE_I2C
 #define EE_HANDS
+
 #define IGNORE_MOD_TAP_INTERRUPT
 #define TAPPING_TERM 150
+
+#define RGBLIGHT_LAYERS
+
+#undef RGBLIGHT_DEFAULT_HUE
+#undef RGBLIGHT_DEFAULT_SAT
+#undef RGBLIGHT_DEFAULT_VAL
+
+#define RGBLIGHT_DEFAULT_HUE 0
+#define RGBLIGHT_DEFAULT_SAT 0
+#define RGBLIGHT_DEFAULT_VAL 0
+
+#undef RGBLIGHT_DEFAULT_MODE
+#define RGBLIGHT_DEFAULT_MODE RGBLIGHT_MODE_STATIC_LIGHT
+
+#undef RGBLIGHT_ANIMATIONS
+#undef RGBLIGHT_EFFECT_BREATHING
+#undef RGBLIGHT_EFFECT_RAINBOW_MOOD
+#undef RGBLIGHT_EFFECT_RAINBOW_SWIRL
+#undef RGBLIGHT_EFFECT_SNAKE
+#undef RGBLIGHT_EFFECT_KNIGHT
+#undef RGBLIGHT_EFFECT_CHRISTMAS
+#undef RGBLIGHT_EFFECT_STATIC_GRADIENT
+#undef RGBLIGHT_EFFECT_RGB_TEST
+#undef RGBLIGHT_EFFECT_ALTERNATING
+#undef RGBLIGHT_EFFECT_TWINKLE
--- keyboards/keebio/iris/keymaps/kchenweijie/keymap.c
+++ keyboards/keebio/iris/keymaps/kchenweijie/keymap.c
@@ -9,6 +9,29 @@
 #define _GAMING_NUMS 6
 #define _GAMING_FNS 7
 
+#define LSFT_A LSFT_T(KC_A)
+#define LCTL_S LCTL_T(KC_S)
+#define LGUI_D LGUI_T(KC_D)
+#define LALT_F LALT_T(KC_F)
+
+#define LALT_J LALT_T(KC_J)
+#define RGUI_K RGUI_T(KC_K)
+#define RCTL_L RCTL_T(KC_L)
+#define RSFT_Q RSFT_T(KC_QUOT)
+
+#define LFUN_E LT(_FUNCTIONS, KC_ESC)
+#define LNUM_T LT(_NUMBERS, KC_TAB)
+#define LSYM_E LT(_SYMBOLS, KC_ENT)
+#define LNAV_B LT(_NAVIGATION, KC_BSPC)
+
+#define LGFN MO(_GAMING_FNS)
+#define LGNM MO(_GAMING_NUMS)
+#define LGNV MO(_NAVIGATION)
+
+#define TGAM TG(_GAMING)
+
+#define _____ KC_NO
+
 enum custom_keycodes {
     QWERTY = SAFE_RANGE,
     GAMING,
@@ -20,14 +43,71 @@ enum custom_keycodes {
     GAMING_FNS,
 };
 
-const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {[_QWERTY]      = LAYOUT(KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_NO, KC_NO, LSFT_T(KC_A), LCTL_T(KC_S), LGUI_T(KC_D), LALT_T(KC_F), KC_G, KC_H, LALT_T(KC_J), RGUI_T(KC_K), RCTL_T(KC_L), RSFT_T(KC_QUOT), KC_NO, KC_NO, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_NO, KC_NO, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_NO, LT(_FUNCTIONS, KC_ESC), LT(_NUMBERS, KC_TAB), LT(_SYMBOLS, KC_ENT), KC_SPC, LT(_NAVIGATION, KC_BSPC), KC_DEL),
-                                                              [_GAMING]      = LAYOUT(KC_ESC, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_NO, KC_LSFT, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_NO, KC_LCTL, KC_Z, KC_X, KC_C, KC_V, KC_B, MO(_GAMING_FNS), KC_NO, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_NO, KC_LALT, KC_SPC, MO(_GAMING_NUMS), KC_NO, MO(_NAVIGATION), KC_NO),
-                                                              [_NUMBERS]     = LAYOUT(KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_GRV, KC_7, KC_8, KC_9, KC_BSLS, KC_NO, KC_NO, KC_LSFT, KC_LCTL, KC_LGUI, KC_LALT, KC_NO, KC_LBRC, KC_4, KC_5, KC_6, KC_RBRC, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_EQL, KC_1, KC_2, KC_3, KC_SCLN, KC_NO, KC_NO, KC_TRNS, KC_NO, KC_MINS, KC_0, KC_DOT),
-                                                              [_SYMBOLS]     = LAYOUT(KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_TILD, KC_AMPR, KC_ASTR, KC_LPRN, KC_PIPE, KC_NO, KC_NO, KC_LSFT, KC_LCTL, KC_LGUI, KC_LALT, KC_NO, KC_LCBR, KC_DLR, KC_PERC, KC_CIRC, KC_RCBR, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_PLUS, KC_EXLM, KC_AT, KC_HASH, KC_COLN, KC_NO, KC_NO, KC_NO, KC_TRNS, KC_UNDS, KC_LPRN, KC_RPRN),
-                                                              [_FUNCTIONS]   = LAYOUT(KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_F7, KC_F8, KC_F9, KC_F12, KC_NO, KC_NO, KC_LSFT, KC_LCTL, KC_LGUI, KC_LALT, KC_NO, KC_NO, KC_F4, KC_F5, KC_F6, KC_F11, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_F1, KC_F2, KC_F3, KC_F10, KC_NO, KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO),
-                                                              [_NAVIGATION]  = LAYOUT(KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, TG(_GAMING), KC_NO, KC_NO, KC_NO, KC_UP, KC_PGUP, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_HOME, KC_LEFT, KC_DOWN, KC_RGHT, KC_END, KC_CAPS, KC_LALT, KC_RGUI, KC_RCTL, KC_RSFT, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_PGDN, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS, KC_NO),
-                                                              [_GAMING_NUMS] = LAYOUT(KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_EQL, KC_7, KC_8, KC_9, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_MINS, KC_4, KC_5, KC_6, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_0, KC_1, KC_2, KC_3, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS, KC_NO, KC_NO, KC_NO),
-                                                              [_GAMING_FNS]  = LAYOUT(KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_F12, KC_F7, KC_F8, KC_F9, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_F11, KC_F4, KC_F5, KC_F6, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_F10, KC_F1, KC_F2, KC_F3, KC_NO, KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO)};
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [_QWERTY] = LAYOUT(
+        _____,  _____,  _____,  _____,  _____,  _____,                      _____,  _____,  _____,  _____,  _____,  _____,
+        _____,  KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,                       KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   _____,
+        _____,  LSFT_A, LCTL_S, LGUI_D, LALT_F, KC_G,                       KC_H,   LALT_J, RGUI_K, RCTL_L, RSFT_Q, _____,
+        _____,  KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   _____,      _____,  KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,_____,
+                                        LFUN_E, LNUM_T, LSYM_E,     KC_SPC, LNAV_B, KC_DEL
+    ),
+                                                              
+    [_GAMING] = LAYOUT(
+        KC_ESC, _____,  _____,  _____,  _____,  _____,                      _____,  _____,  _____,  _____,  _____,  _____,
+        KC_TAB, KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,                       KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   _____,
+        KC_LSFT,KC_A,   KC_S,   KC_D,   KC_F,   KC_G,                       KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,_____,
+        KC_LCTL,KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   LGFN,       _____,  KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,_____,
+                                        KC_LALT,KC_SPC, LGNM,       _____,  LGNV,   _____
+    ),
+
+    [_NUMBERS] = LAYOUT(
+        _____,  _____,  _____,  _____,  _____,  _____,                      _____,  _____,  _____,  _____,  _____,  _____,
+        _____,  _____,  _____,  _____,  _____,  _____,                      KC_GRV, KC_7,   KC_8,   KC_9,   KC_BSLS,_____,
+        _____,  KC_LSFT,KC_LCTL,KC_LGUI,KC_LALT,_____,                      KC_LBRC,KC_4,   KC_5,   KC_6,   KC_RBRC,_____,
+        _____,  _____,  _____,  _____,  _____,  _____,  _____,      _____,  KC_EQL, KC_1,   KC_2,   KC_3,   KC_SCLN,_____,
+                                        _____,  KC_TRNS,_____,      KC_MINS,KC_0,   KC_DOT
+    ),
+
+    [_SYMBOLS] = LAYOUT(
+        _____,  _____,  _____,  _____,  _____,  _____,                      _____,  _____,  _____,  _____,  _____,  _____,
+        _____,  _____,  _____,  _____,  _____,  _____,                      KC_TILD,KC_AMPR,KC_ASTR,KC_LPRN,KC_PIPE,_____,
+        _____,  KC_LSFT,KC_LCTL,KC_LGUI,KC_LALT,_____,                      KC_LCBR,KC_DLR, KC_PERC,KC_CIRC,KC_RCBR,_____,
+        _____,  _____,  _____,  _____,  _____,  _____,  _____,      _____,  KC_PLUS,KC_EXLM,KC_AT,  KC_HASH,KC_COLN,_____,
+                                        _____,  _____,  KC_TRNS,    KC_UNDS,KC_LPRN,KC_RPRN
+    ),
+
+    [_FUNCTIONS] = LAYOUT(
+        _____,  _____,  _____,  _____,  _____,  _____,                      _____,  _____,  _____,  _____,  _____,  _____,
+        _____,  _____,  _____,  _____,  _____,  _____,                      _____,  KC_F7,  KC_F8,  KC_F9,  KC_F12, _____,
+        _____,  KC_LSFT,KC_LCTL,KC_LGUI,KC_LALT,_____,                      _____,  KC_F4,  KC_F5,  KC_F6,  KC_F11, _____,
+        _____,  _____,  _____,  _____,  _____,  _____,  _____,      _____,  _____,  KC_F1,  KC_F2,  KC_F3,  KC_F10, _____,
+                                        KC_TRNS,_____,  _____,      _____,  _____,  _____
+    ),
+
+    [_NAVIGATION] = LAYOUT(
+        _____,  _____,  _____,  _____,  _____,  _____,                      _____,  _____,  _____,  _____,  _____,  TGAM,
+        _____,  _____,  _____,  KC_UP,  KC_PGUP,_____,                      _____,  _____,  _____,  _____,  _____,  _____,
+        _____,  KC_HOME,KC_LEFT,KC_DOWN,KC_RGHT,KC_END,                     KC_CAPS,KC_LALT,KC_RGUI,KC_RCTL,KC_RSFT,_____,
+        _____,  _____,  _____,  _____,  KC_PGDN,_____,  _____,      _____,  _____,  _____,  _____,  _____,  _____,  _____,
+                                        _____,  _____,  _____,      _____,  KC_TRNS,_____
+    ),
+
+    [_GAMING_NUMS] = LAYOUT(
+        _____,  _____,  _____,  _____,  _____,  _____,                      _____,  _____,  _____,  _____,  _____,  _____,
+        _____,  KC_EQL, KC_7,   KC_8,   KC_9,   _____,                      _____,  _____,  _____,  _____,  _____,  _____,
+        _____,  KC_MINS,KC_4,   KC_5,   KC_6,   _____,                      _____,  _____,  _____,  _____,  _____,  _____,
+        _____,  KC_0,   KC_1,   KC_2,   KC_3,   _____,  _____,      _____,  _____,  _____,  _____,  _____,  _____,  _____,
+                                        _____,  _____,  KC_TRNS,    _____,  _____,  _____
+    ),
+
+    [_GAMING_FNS] = LAYOUT(
+        _____,  _____,  _____,  _____,  _____,  _____,                      _____,  _____,  _____,  _____,  _____,  _____,
+        _____,  KC_F12, KC_F7,  KC_F8,  KC_F9,  _____,                      _____,  _____,  _____,  _____,  _____,  _____,
+        _____,  KC_F11, KC_F4,  KC_F5,  KC_F6,  _____,                      _____,  _____,  _____,  _____,  _____,  _____,
+        _____,  KC_F10, KC_F1,  KC_F2,  KC_F3,  _____,  KC_TRNS,    _____,  _____,  _____,  _____,  _____,  _____,  _____,
+                                        _____,  _____,  _____,      _____,  _____,  _____
+    )
+};
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     return true;
@@ -38,23 +118,5 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
 }
 
 layer_state_t layer_state_set_user(layer_state_t state) {
-    switch (get_highest_layer(state)) {
-        case _QWERTY:
-            break;
-        case _GAMING:
-            break;
-        case _NUMBERS:
-            break;
-        case _SYMBOLS:
-            break;
-        case _FUNCTIONS:
-            break;
-        case _NAVIGATION:
-            break;
-        case _GAMING_NUMS:
-            break;
-        case _GAMING_FNS:
-            break;
-    }
     return state;
 }
